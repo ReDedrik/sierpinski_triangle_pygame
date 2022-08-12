271eee5ecb7c53b525c6a1060ddb3b112ad3cc1c
ReDedrik <redmanesh@gmail.com>
2022-08-11 21:27:19 -0500

    NOT FINISHED. completed, square, pentagon started

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from math import sqrt
+import math
 import random as rd
 import time
 
@@ -10,6 +10,7 @@ pygame.init()
 ### creating canvas and establishing variables
 ###
 i = 0
+previous_point = -1
 dots = 30000
 dotsize = 1
 height, width = 800, 800
@@ -27,6 +28,7 @@ screen.fill(white)
 
 def dot(coords):
     pygame.draw.circle(screen, black, coords, 1)
+
 ###
 ### Endpoint class 
 ###
@@ -39,11 +41,6 @@ class Endpoint:
         self.coords = (x, y)
         Endpoint.instances.append(self)
     
-    @staticmethod
-    def random_vertex():
-        num = rd.randint(1, len(Endpoint.instances) - 1)
-        return Endpoint.instances[num]
-
     def Dot(self):
         pygame.draw.circle(screen, black, self.coords, 2)
 
@@ -56,9 +53,9 @@ class Endpoint:
     @staticmethod
     def equilateral_triangle(length):
         center_of_screen = (width / 2, height / 2)
-        pt1 = Endpoint(center_of_screen[0] - length / 2, center_of_screen[1] + (sqrt(3) / 2) * (1/2) * length)
-        pt2 = Endpoint(center_of_screen[0] + length / 2, center_of_screen[1] + (sqrt(3) / 2) * (1/2) * length)
-        pt3 = Endpoint(center_of_screen[0], center_of_screen[1] - (sqrt(3) / 2) * (1/2) * length)
+        pt1 = Endpoint(center_of_screen[0] - length / 2, center_of_screen[1] + (math.sqrt(3) / 2) * (1/2) * length)
+        pt2 = Endpoint(center_of_screen[0] + length / 2, center_of_screen[1] + (math.sqrt(3) / 2) * (1/2) * length)
+        pt3 = Endpoint(center_of_screen[0], center_of_screen[1] - (math.sqrt(3) / 2) * (1/2) * length)
         for i in Endpoint.instances:
             i.Dot()
             Endpoint.Line(i, Endpoint.instances[(Endpoint.instances.index(i) + 1) % 3])
@@ -68,12 +65,27 @@ class Endpoint:
         center_of_screen = (width / 2, height / 2)
         s_pt1 = Endpoint(center_of_screen[0] - length / 2, center_of_screen[1] + length / 2)
         s_pt2 = Endpoint(center_of_screen[0] + length / 2, center_of_screen[1] + length / 2)
-        s_pt3 = Endpoint(center_of_screen[0] - length / 2, center_of_screen[1] - length / 2)
-        s_pt4 = Endpoint(center_of_screen[0] + length / 2, center_of_screen[1] - length / 2)
+        s_pt3 = Endpoint(center_of_screen[0] + length / 2, center_of_screen[1] - length / 2)
+        s_pt4 = Endpoint(center_of_screen[0] - length / 2, center_of_screen[1] - length / 2)
         for i in Endpoint.instances:
             i.Dot()
-        Endpoint.Line(s_pt1, s_pt2, s_pt3)
-        Endpoint.Line(s_pt4, s_pt2, s_pt3)
+        Endpoint.Line(s_pt1, s_pt2, s_pt4)
+        Endpoint.Line(s_pt3, s_pt2, s_pt4)
+
+    def pentagon(length):
+        center_of_screen = (width / 2, height / 2)
+        h = (length / 2) / math.cos(54 * math.pi / 180)
+        p_pt1 = Endpoint(center_of_screen[0] - h * math.cos(54 * math.pi / 180), center_of_screen[1] - h * math.sin(-54 * math.pi / 180))
+        p_pt2 = Endpoint(center_of_screen[0] + h * math.cos(-54 * math.pi / 180), center_of_screen[1] - h * math.sin(-54 * math.pi / 180))
+        p_pt5 = Endpoint(center_of_screen[0] - h * math.cos(18 * math.pi / 180), center_of_screen[1] - h * math.sin(18 * math.pi / 180))
+        p_pt4 = Endpoint(center_of_screen[0], center_of_screen[1] - h)
+        p_pt3 = Endpoint(center_of_screen[0] + h * math.cos(-18 * math.pi / 180), center_of_screen[1] - h * math.sin(18 * math.pi / 180))
+        Endpoint.Line(p_pt1, p_pt2, p_pt5)
+        Endpoint.Line(p_pt3, p_pt2, p_pt4)
+        Endpoint.Line(p_pt4, p_pt5)
+        for i in Endpoint.instances:
+            i.Dot()
+        
 ###
 ### function to pick first random point
 ###
@@ -81,9 +93,9 @@ def first_point(list_of_pts):
     if len(list_of_pts) == 4:
         return (rd.randint(list_of_pts[0].coords[0], list_of_pts[1].coords[0]), rd.randint(list_of_pts[3].coords[1], list_of_pts[0].coords[1]))
     else:
-        pt1 = list_of_pts[0]
-        pt2 = list_of_pts[1]
-        pt3 = list_of_pts[2]
+        pt1 = list_of_pts[0].coords
+        pt2 = list_of_pts[1].coords
+        pt3 = list_of_pts[2].coords
         x, y = sorted([rd.random(), rd.random()])
         s, t, u = x, y - x, 1 - y
         return (s * pt1[0] + t * pt2[0] + u * pt3[0],
@@ -97,8 +109,25 @@ def draw_new_point(point1, point2):
     y_value = (point1[1] + point2[1]) / 2
     return (x_value, y_value)
 
-Endpoint.square(400)
-#Endpoint.equilateral_triangle(600)
+def pick_random_vertex(vertex):
+    num_list = []
+    for i in range(len(Endpoint.instances)):
+        num_list.append(i)
+    if len(Endpoint.instances) == 4:
+        num_list.remove(vertex)
+        previous_point = rd.choice(num_list)
+        return previous_point
+    if len(Endpoint.instances) == 3:
+        return rd.randint(0, len(Endpoint.instances) - 1)
+    else:
+        num_list.remove(num_list[(vertex + 1) % (len(num_list))])
+        num_list.remove(num_list[(vertex + (len(num_list) - 1)) % len(num_list)])
+        return rd.choice(num_list)
+
+
+#Endpoint.pentagon(350)
+#Endpoint.square(700)
+Endpoint.equilateral_triangle(600)
 pygame.display.update()
 
 fp = first_point(Endpoint.instances)
@@ -108,14 +137,15 @@ pygame.display.update()
 ###
 ### game loop (update)
 ###
+vertex = 1
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     if i <= 10:
         time.sleep(0.3)
     if i <= dots:
-        vertex = Endpoint.random_vertex()
-        next_point = draw_new_point(next_point, vertex.coords)
+        vertex = pick_random_vertex(vertex)
+        next_point = draw_new_point(next_point, Endpoint.instances[vertex].coords)
         dot(next_point)
     i += 1
     pygame.display.update()
